77464aa00f2db4562046d8061fa1d97db68e985c
Caroline TRI <caroline.tri77@gmail.com>
2018-12-18 16:04:07 +0100

    ajout tableau

--- src/app/dash-board-component/dash-board-component.component.ts
+++ src/app/dash-board-component/dash-board-component.component.ts
@@ -10,11 +10,13 @@ import { HttpClient } from '@angular/common/http';
 @Injectable()
 export class DashBoardComponentComponent implements OnInit {
 
-  
-  lastUpdate = new Date();
+  results = [];
+  resultsFilter = [];
+  compteur = 0;
 
   constructor(private http: HttpClient) {
-    let results = [];
+    
+    
   }
 
   ngOnInit() {
@@ -26,18 +28,60 @@ export class DashBoardComponentComponent implements OnInit {
       () => {
         this.getApiService();
       }
-    , 60000)
+    , 10000)
   }
 
   getApiService() {
     const proxyurl = "https://cors-anywhere.herokuapp.com/";
-    let tempObj = {};
-    let upDate = new Date();
+    let tempObj = {
+      tempTime: Date,
+      rel: String,
+      diff: Date,
+    };
+    
+  
+    let debut = Date.now();
+
+    let timeShow = new Date();
+    let dd = timeShow.getDate();
+    let mm = timeShow.getMonth() + 1;
+    let yyyy = timeShow.getFullYear();
+    let hh = timeShow.getHours();
+    let min = timeShow.getMinutes();
+    let ss = timeShow.getSeconds();
+    if (dd < 10) {
+      dd = 0 + dd;
+    } 
+    if (mm < 10) {
+      mm = 0 + mm;
+    }
+
+    tempObj.tempTime = dd + '/' + mm + '/' + yyyy + ' ' + hh + ':' + min + ':' + ss;
+
     this.http
       .get<any[]>(proxyurl+'https://prod.geev.fr/alive')
       .subscribe(
         (response) => {
-          tempObj = {...tempObj, upDate};
+          this.compteur += 1;
+          tempObj.rel = this.compteur + 'm ago';
+          let fin = Date.now();
+          let diff = fin - debut;
+          tempObj.diff = diff;
+          this.results = [...this.results, tempObj];
+          if (this.results.length <= 5) {
+            this.resultsFilter = this.results;
+          } else {
+            let j = 0;
+            for (let i = this.results.length - 5;i<= this.results.length;i++) {
+              this.resultsFilter[j] = this.results[i];
+              if (i !== this.results.length) {
+                j += 1;
+              }    
+            }
+
+          }
+
+          
         },
         (error) => {
           console.log('Erreur ! : ' + error);
